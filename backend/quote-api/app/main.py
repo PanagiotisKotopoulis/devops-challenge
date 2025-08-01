2f49f10aca56a723d1a19e6bebc3582b77f9bc5b
Panagiotis Kotopoulis <91201862+PanagiotisKotopoulis@users.noreply.github.com>
2025-08-01 15:51:58 +0300

    Update also here with the actual url since i previously it didnt allow it

--- backend/quote-api/app/main.py
+++ backend/quote-api/app/main.py
@@ -1,19 +1,10 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.quotes import get_random_quote
-
-app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001",
-                   "http://localhost:3001"
+    allow_origins=[
+        "http://localhost:3001",  # optional for local testing
+        "https://frontend-app--5mgubcy.wittydesert-2d99a67b.westeurope.azurecontainerapps.io"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.get("/quote")
-def quote():
-    return {"quote": get_random_quote()}
