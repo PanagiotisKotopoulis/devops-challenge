57f28dcc232f032f380c1e4b7a7976afc65d4e5c
Panagiotis Kotopoulis <91201862+PanagiotisKotopoulis@users.noreply.github.com>
2025-08-01 16:06:45 +0300

    Correct the file since from last commit i cut  by mistake some necessary lines

--- backend/quote-api/app/main.py
+++ backend/quote-api/app/main.py
@@ -1,10 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.quotes import get_random_quote
+
+app = FastAPI()  # 👈 this must come before using 'app'
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3001",  # optional for local testing
+        "http://localhost:3001",
         "https://frontend-app--5mgubcy.wittydesert-2d99a67b.westeurope.azurecontainerapps.io"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/quote")
+def quote():
+    return {"quote": get_random_quote()}
